7d1df5376e0b6c9d21930655b999246a2acb27b3
Hhertilien1 <Kinghertilien@gmail.com>
2025-02-08 09:57:54 -0500

    almost have access to all pages and can now move forward with converting and putting pages together

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
+import AdminDash from "./Components/AdminDash";
+import AdminUpdateProfile from "./Components/AdminUpdateProfile";
+import CreateEmployee from "./Components/CreateEmployee";
+import CreateJob from "./Components/CreateJob";
+import EmployeeDash from "./Components/EmployeeDash"; // Fix incorrect import
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import UpdateProfile from "./Components/UpdateProfile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -8,20 +16,32 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-              <Navbar.Brand>Kitchen Saver</Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/AdminDash">Admin Dash</Nav.Link>
-                <Nav.Link as={Link} to="/CreateJob">Create Job</Nav.Link>
-                <Nav.Link as={Link} to="/CreateEmployee">Create Employee</Nav.Link>
-                <Nav.Link as={Link} to="/UpdateProfile">Update Profile</Nav.Link>
-              </Nav>
-              <Button variant="danger" as={Link} to="/logout">Logout</Button>
-            </Container>
-          </Navbar>
-        </header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Kitchen Saver</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/admindash">Admin Dash</Nav.Link>
+              <Nav.Link as={Link} to="/createjob">Create Job</Nav.Link>
+              <Nav.Link as={Link} to="/createemployee">Create Employee</Nav.Link>
+              <Nav.Link as={Link} to="/updateprofile">Update Profile</Nav.Link>
+            </Nav>
+            <Button variant="danger" as={Link} to="/logout">Logout</Button>
+          </Container>
+        </Navbar>
+
+        {/* Define routes for pages */}
+        <Routes>
+          <Route path="/admindash" element={<AdminDash />} />
+          <Route path="/createjob" element={<CreateJob />} />
+          <Route path="/createemployee" element={<CreateEmployee />} />
+          <Route path="/updateprofile" element={<UpdateProfile />} />
+          <Route path="/adminupdateprofile" element={<AdminUpdateProfile />} />
+          <Route path="/employeedash" element={<EmployeeDash />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* Default Route */}
+          <Route path="/" element={<Login />} />
+        </Routes>
       </div>
     </Router>
   );
